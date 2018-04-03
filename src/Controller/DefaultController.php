ffd2c066aa70b270620d33ae6a861c6671c3e9cb
SkillUP Student <student@skillup.spb.ru>
2018-04-03 22:19:15 +0300

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('add.html.twig', [
+            'test' => 123,
+        ]);
+    }
+}
